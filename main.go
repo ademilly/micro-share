4ae0be26ef046d8b796f7a81728e171717f5f735
Aurélien Demilly <demilly.aurelien@gmail.com>
2018-05-30 15:55:40 +0200

    add naive login feature

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 // TODO:
-// - add auth
+// - add bdd for auth
+// - add "New User" feature
+// - add user rights management
+// - add file mapping
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -13,6 +17,8 @@ import (
 	"os"
 	"path"
 
+	"github.com/ademilly/micro-share/auth"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	"github.com/gorilla/handlers"
 )
 
@@ -23,7 +29,7 @@ var (
 	keyFile  string
 )
 
-func writeTo(text string, w http.ResponseWriter) {
+func writeTo(w http.ResponseWriter, text string) {
 	w.Header().Set("Content-Type", "text/text")
 	if _, err := w.Write([]byte(text)); err != nil {
 		log.Printf("could not write to http.ResponseWriter: %v", err)
@@ -38,8 +44,7 @@ func get(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("could not read file %s: %v", filename, err)
 
-		w.WriteHeader(http.StatusNotFound)
-		writeTo("file does not exist :(", w)
+		http.Error(w, "file does not exist :(", http.StatusNotFound)
 		return
 	}
 
@@ -53,8 +58,50 @@ func get(w http.ResponseWriter, r *http.Request) {
 		w.Header().Del("Content-Type")
 		w.Header().Del("Content-Disposition")
 
-		w.WriteHeader(http.StatusInternalServerError)
-		writeTo("something failed :(", w)
+		http.Error(w, "file transfer failed on our side :(", http.StatusInternalServerError)
+	}
+}
+
+func login(tokenizer func(string) (string, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+
+		var candidate auth.User
+		err := decoder.Decode(&candidate)
+
+		if err != nil {
+			log.Printf("could not read request body: %v", err)
+
+			http.Error(w, `request should be { "username": "USERNAME", "password": "PASSWORD" }`, http.StatusBadRequest)
+			return
+		}
+
+		err = auth.CheckHash(candidate, func() (auth.User, error) {
+			return auth.Hash(candidate)
+		})
+		if err != nil {
+			log.Println("candidate does not provide valid user / password couple")
+
+			http.Error(w, "this is not a valid user / password", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := tokenizer(candidate.Username)
+		if err != nil {
+			log.Printf("could not obtain token for user %s: %v", candidate.Username, err)
+
+			http.Error(w, fmt.Sprintf("could not obtain token for user %s :(", candidate.Username), http.StatusInternalServerError)
+			return
+		}
+
+		writeTo(w, token)
+	}
+}
+
+func protect(tokenMiddleware *jwtmiddleware.JWTMiddleware, handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenMiddleware.Handler(http.HandlerFunc(handler)).ServeHTTP(w, r)
 	}
 }
 
@@ -69,13 +116,22 @@ func main() {
 		log.Fatalf("can't list directory %s: %v", root, err)
 	}
 
+	jwtKey := os.Getenv("JWT_KEY")
+	if jwtKey == "" {
+		log.Fatalf("JWT_KEY environment variable has not been set; it is needed for authentication to work")
+	}
+
+	tokenMiddleware := auth.TokenMiddleware(jwtKey)
+
 	addr := fmt.Sprintf("0.0.0.0:%s", port)
 	handler := http.NewServeMux()
 
-	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		writeTo("welcome to this micro-share instance ;)", w)
-	})
-	handler.HandleFunc("/get/", get)
+	handler.HandleFunc("/login", login(auth.Tokenizer(addr, jwtKey)))
+
+	handler.HandleFunc("/", protect(tokenMiddleware, func(w http.ResponseWriter, r *http.Request) {
+		writeTo(w, "welcome to this micro-share instance ;)")
+	}))
+	handler.HandleFunc("/get/", protect(tokenMiddleware, get))
 
 	srv := http.Server{
 		Handler: handlers.LoggingHandler(os.Stdout, handler),
