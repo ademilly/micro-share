b8d32acab0b999a23a07ef63e8d774a8c341b9ff
Aurélien Demilly <demilly.aurelien@gmail.com>
2018-08-31 10:21:24 +0200

    update postgres schema to prepare user right management

--- psql/schema.sql
+++ psql/schema.sql
@@ -2,9 +2,49 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
-    passhash TEXT
+    passhash TEXT,
+    CHECK (username <> '')
 );
-CREATE INDEX on users (id);
+CREATE INDEX ON users (id);
 
 -- User insertion example
 -- INSERT INTO users (username, passhash) VALUES ('default_user', 'somevalidhash');
+
+DROP TABLE IF EXISTS groups CASCADE;
+CREATE TABLE groups (
+    id BIGSERIAL PRIMARY KEY,
+    groupname TEXT UNIQUE NOT NULL,
+    CHECK (groupname <> '')
+);
+CREATE INDEX ON groups (id);
+
+DROP TABLE IF EXISTS relations CASCADE;
+CREATE TABLE relations (
+    id BIGSERIAL PRIMARY KEY,
+    groupID BIGSERIAL NOT NULL REFERENCES groups (id) ON UPDATE CASCADE,
+    userID BIGSERIAL NOT NULL REFERENCES users (id) ON UPDATE CASCADE
+);
+CREATE INDEX ON relations (id);
+CREATE INDEX ON relations (groupID);
+CREATE INDEX ON relations (userID);
+
+DROP TABLE IF EXISTS files CASCADE;
+CREATE TABLE files (
+    id BIGSERIAL PRIMARY KEY,
+    filepath TEXT UNIQUE NOT NULL,
+    md5 TEXT NOT NULL,
+    ownerID BIGSERIAL NOT NULL REFERENCES groups (id) ON UPDATE CASCADE,
+    CHECK (filepath <> '' and md5 <> '')
+);
+CREATE INDEX ON files (id);
+CREATE INDEX ON files (ownerID);
+
+DROP TABLE IF EXISTS readers CASCADE;
+CREATE TABLE readers (
+    id BIGSERIAL PRIMARY KEY,
+    fileID BIGSERIAL NOT NULL REFERENCES files (id) ON UPDATE CASCADE,
+    groupID BIGSERIAL NOT NULL REFERENCES groups (id) ON UPDATE CASCADE
+);
+CREATE INDEX ON readers (id);
+CREATE INDEX ON readers (fileID);
+CREATE INDEX ON readers (groupID);
