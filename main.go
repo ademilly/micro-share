f0c64a7f7dbcc39ee1dbcc4de59d3037b08e290f
Aurélien Demilly <demilly.aurelien@gmail.com>
2018-08-31 10:27:31 +0200

    add support for list and uploading files

--- main.go
+++ main.go
@@ -12,11 +12,15 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path"
+	"path/filepath"
 	"strconv"
+	"strings"
+
+	"github.com/kennygrant/sanitize"
 
 	"github.com/ademilly/auth"
 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/handlers"
 )
 
@@ -33,20 +37,79 @@ var (
 	dbname     string
 	dbhostname string
 	dbport     string
+	dbconf     ConnConf
 
 	httpGET  = []string{http.MethodOptions, http.MethodGet}
 	httpPOST = []string{http.MethodOptions, http.MethodPost}
+
+	tokenMiddleware *jwtmiddleware.JWTMiddleware
 )
 
-func get(w http.ResponseWriter, r *http.Request) {
-	filename := path.Join(root, r.URL.Path[len("/get/"):])
+const maxUploadSize = 2 * 1024 * 1024 // 2Go
 
-	f, err := os.Open(filename)
-	defer f.Close()
+func handleError(err error, w http.ResponseWriter, code int, format string, a ...interface{}) (func(), error) {
 	if err != nil {
-		msg := fmt.Sprintf("could not read file %s: %v", filename, err)
-		log.Println(msg)
-		http.Error(w, msg, http.StatusBadRequest)
+		return func() {
+			msg := fmt.Sprintf(format, a...)
+			log.Println(msg)
+			http.Error(w, msg, code)
+		}, err
+	}
+	return nil, nil
+}
+
+func usernameFromRequestToken(r *http.Request) (string, error) {
+	tokenString, err := tokenMiddleware.Options.Extractor(r)
+	if err != nil {
+		return "", fmt.Errorf("could not extract token string from request: %v", err)
+	}
+
+	token, err := jwt.Parse(tokenString, tokenMiddleware.Options.ValidationKeyGetter)
+	if err != nil {
+		return "", fmt.Errorf("could not parse token string: %v", err)
+	}
+	return token.Claims.(jwt.MapClaims)["user"].(string), nil
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	username, err := usernameFromRequestToken(r)
+	if f, _ := handleError(err, w, http.StatusBadRequest, "could not obtain username from token: %v", err); err != nil {
+		f()
+		return
+	}
+
+	filedata, err := listFiles(dbconf, username)
+	if f, _ := handleError(err, w, http.StatusInternalServerError, "could not retrieve files: %v", err); err != nil {
+		f()
+		return
+	}
+
+	results := []string{}
+	for _, data := range filedata {
+		results = append(results, fmt.Sprintf("%s - %s", data.Path, data.MD5))
+	}
+
+	w.Write([]byte(strings.Join(results, "\n")))
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	username, err := usernameFromRequestToken(r)
+	if f, _ := handleError(err, w, http.StatusBadRequest, "could not obtain username from token: %v", err); err != nil {
+		f()
+		return
+	}
+
+	md5hash := r.URL.Path[len("/get/"):]
+	filepath, err := getFilepath(dbconf, md5hash, username)
+	if f, _ := handleError(err, w, http.StatusInternalServerError, "could not obtain filepath: %v", err); err != nil {
+		f()
+		return
+	}
+
+	f, err := os.Open(filepath)
+	defer f.Close()
+	if f, _ := handleError(err, w, http.StatusBadRequest, "could not read file %s: %v", filepath, err); err != nil {
+		f()
 		return
 	}
 
@@ -54,31 +117,68 @@ func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", f.Name()))
 	_, err = io.Copy(w, f)
 
-	if err != nil {
+	if f, _ := handleError(err, w, http.StatusInternalServerError, "could not write file to client %s: %v", filepath, err); err != nil {
 		w.Header().Del("Content-Type")
 		w.Header().Del("Content-Disposition")
-
-		msg := fmt.Sprintf("could not write file to client %s: %v", filename, err)
-		log.Println(msg)
-		http.Error(w, msg, http.StatusInternalServerError)
+		f()
 		return
 	}
 }
 
-func newUser(w http.ResponseWriter, r *http.Request) {
-	candidate, err := auth.UserFromRequest(r)
-	if err != nil {
-		msg := fmt.Sprintf("could not retrieve candidate User from request: %v", err)
-		log.Println(msg)
-		http.Error(w, msg, http.StatusBadRequest)
+func upload(w http.ResponseWriter, r *http.Request) {
+	username, err := usernameFromRequestToken(r)
+	if f, _ := handleError(err, w, http.StatusBadRequest, "could not obtain username from token: %v", err); err != nil {
+		f()
 		return
 	}
 
-	userID, err := addUser(dbuser, dbpassword, dbname, dbhostname, dbport, candidate)
-	if err != nil {
-		msg := fmt.Sprintf("could not create a new user: %v", err)
-		log.Println(msg)
-		http.Error(w, msg, http.StatusInternalServerError)
+	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+	if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+		f, _ := handleError(err, w, http.StatusBadRequest, "file is too big: %v", err)
+		f()
+		return
+	}
+
+	file, handler, err := r.FormFile("uploadFile")
+	if f, err := handleError(err, w, http.StatusBadRequest, "file is invalid: %v", err); err != nil {
+		f()
+		return
+	}
+	defer file.Close()
+
+	newPath := filepath.Join(root, sanitize.Name(handler.Filename))
+	newFile, err := os.Create(newPath)
+	if f, err := handleError(err, w, http.StatusInternalServerError, "could not write file: %v", err); err != nil {
+		f()
+		return
+	}
+	defer newFile.Close()
+
+	_, err = io.Copy(newFile, file)
+	if f, err := handleError(err, w, http.StatusInternalServerError, "could not write file: %v", err); err != nil || newFile.Close() != nil {
+		f()
+		return
+	}
+
+	md5String, err := addFile(dbconf, newPath, username)
+	if f, err := handleError(err, w, http.StatusInternalServerError, "could not register file: %v", err); err != nil {
+		f()
+		return
+	}
+
+	w.Write([]byte(md5String))
+}
+
+func newUser(w http.ResponseWriter, r *http.Request) {
+	candidate, err := auth.UserFromRequest(r)
+	if f, err := handleError(err, w, http.StatusBadRequest, "could not retrieve candidate User from request: %v", err); err != nil {
+		f()
+		return
+	}
+
+	userID, err := addUser(dbconf, candidate)
+	if f, err := handleError(err, w, http.StatusInternalServerError, "could not create a new user: %v", err); err != nil {
+		f()
 		return
 	}
 
@@ -87,51 +187,44 @@ func newUser(w http.ResponseWriter, r *http.Request) {
 
 func login(w http.ResponseWriter, r *http.Request) {
 	candidate, err := auth.UserFromRequest(r)
-	if err != nil {
-		msg := fmt.Sprintf("could not retrieve candidate User from request: %v", err)
-		log.Println(msg)
-		http.Error(w, msg, http.StatusBadRequest)
+	if f, err := handleError(err, w, http.StatusBadRequest, "could not retrieve candidate User from request: %v", err); err != nil {
+		f()
 		return
 	}
 
 	token, err := auth.Login(
 		auth.Tokenizer(addr, jwtKey),
-		retrieve(dbuser, dbpassword, dbname, dbhostname, dbport, candidate.Username),
+		retrieve(dbconf, candidate.Username),
 	)(candidate)
-	if err != nil {
-		msg := fmt.Sprintf("could not obtain token for user %s: %v", candidate.Username, err)
-		log.Println(msg)
-		http.Error(w, msg, http.StatusInternalServerError)
+	if f, err := handleError(err, w, http.StatusInternalServerError, "could not obtain token for user %s: %v", candidate.Username, err); err != nil {
+		f()
 		return
 	}
 
 	w.Write([]byte(token))
 }
 
-func isInArray(s string, arr []string) bool {
+func isInArray(s string, arr []string) error {
 	stringMap := map[string]bool{}
 	for _, str := range arr {
 		stringMap[str] = true
 	}
 
-	return stringMap[s]
+	if stringMap[s] {
+		return nil
+	}
+	return fmt.Errorf("%s is not in %v", s, arr)
 }
 
 func allow(methods []string, handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if !isInArray(r.Method, methods) {
-			msg := fmt.Sprintf("%s is not in allowed methods: %v", methods, r.Method)
-			http.Error(w, msg, http.StatusBadRequest)
+		err := isInArray(r.Method, methods)
+		if f, err := handleError(err, w, http.StatusBadRequest, "%s is not in allowed methods: %v", methods, r.Method); err != nil {
+			f()
 			return
 		}
 
 		handler.ServeHTTP(w, r)
-	}
-}
-
-func protect(tokenMiddleware *jwtmiddleware.JWTMiddleware, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		tokenMiddleware.Handler(http.HandlerFunc(handler)).ServeHTTP(w, r)
 	}
 }
 
@@ -161,6 +254,8 @@ func init() {
 
 func main() {
 	flag.Parse()
+	dbconf = ConnConf{dbuser, dbpassword, dbname, dbhostname, dbport}
+
 	jwtKey = os.Getenv("JWT_KEY")
 	if jwtKey == "" {
 		log.Fatalf("JWT_KEY environment variable has not been set; it is needed for authentication to work")
@@ -170,7 +265,7 @@ func main() {
 		log.Fatalf("can't list directory %s: %v", root, err)
 	}
 
-	tokenMiddleware := auth.TokenMiddleware(jwtKey)
+	tokenMiddleware = auth.TokenMiddleware(jwtKey)
 
 	addr = fmt.Sprintf("0.0.0.0:%s", port)
 	handler := http.NewServeMux()
@@ -181,7 +276,10 @@ func main() {
 	handler.HandleFunc("/", allow(httpGET, func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome to this micro-share!"))
 	}))
-	handler.HandleFunc("/get/", allow(httpGET, protect(tokenMiddleware, get)))
+
+	handler.HandleFunc("/list", allow(httpGET, auth.Protect(tokenMiddleware, list)))
+	handler.HandleFunc("/upload", allow(httpPOST, auth.Protect(tokenMiddleware, upload)))
+	handler.HandleFunc("/get/", allow(httpGET, auth.Protect(tokenMiddleware, get)))
 
 	srv := http.Server{
 		Handler: handlers.LoggingHandler(os.Stdout, handler),
--- psql.go
+++ psql.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"crypto/md5"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"io"
 
 	"github.com/ademilly/auth"
 	_ "github.com/lib/pq"
 )
 
-func conn(pguser, pgpassword, pgdbname, hostname, port string) (*sql.DB, error) {
-	return sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", pguser, pgpassword, hostname, port, pgdbname))
+// ConnConf wraps necessary variable to build conn string to db
+type ConnConf struct {
+	User     string
+	Password string
+	DBName   string
+	Hostname string
+	Port     string
 }
 
-func retrieve(pguser, pgpassword, pgdbname, hostname, port, username string) auth.UserClosure {
+func (c ConnConf) String() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", c.User, c.Password, c.Hostname, c.Port, c.DBName)
+}
+
+func (c ConnConf) conn() (*sql.DB, error) {
+	return sql.Open("postgres", c.String())
+}
+
+func retrieve(conf ConnConf, username string) auth.UserClosure {
 	return func() (auth.User, error) {
-		db, err := conn(pguser, pgpassword, pgdbname, hostname, port)
+		db, err := conf.conn()
 		if err != nil {
 			return auth.User{}, fmt.Errorf("could not connect to database: %v", err)
 		}
@@ -26,13 +42,13 @@ func retrieve(pguser, pgpassword, pgdbname, hostname, port, username string) aut
 	}
 }
 
-func addUser(pguser, pgpassword, pgdbname, hostname, port string, candidate auth.User) (int64, error) {
+func addUser(conf ConnConf, candidate auth.User) (int64, error) {
 	newUser, err := auth.Hash(candidate)
 	if err != nil {
 		return 0, fmt.Errorf("could not hash candidate password: %v", err)
 	}
 
-	db, err := conn(pguser, pgpassword, pgdbname, hostname, port)
+	db, err := conf.conn()
 	if err != nil {
 		return 0, fmt.Errorf("could not connect to database: %v", err)
 	}
@@ -40,8 +56,111 @@ func addUser(pguser, pgpassword, pgdbname, hostname, port string, candidate auth
 	var userID int64
 	err = db.QueryRow("INSERT INTO users (username, passhash) VALUES ($1, $2) RETURNING id", newUser.Username, newUser.Hash).Scan(&userID)
 	if err != nil {
-		return 0, fmt.Errorf("could not add new user: %v", err)
+		return 0, fmt.Errorf("could not add new user %s: %v", newUser.Username, err)
+	}
+
+	var groupID int64
+	err = db.QueryRow("INSERT INTO groups (groupname) VALUES ($1) RETURNING id", newUser.Username).Scan(&groupID)
+	if err != nil {
+		return 0, fmt.Errorf("could not create user group %s: %v", newUser.Username, err)
+	}
+
+	var relationID int64
+	err = db.QueryRow("INSERT INTO relations (groupID, userID) VALUES ($1, $2) RETURNING id", groupID, userID).Scan(&relationID)
+	if err != nil {
+		return 0, fmt.Errorf("could not create relation groupID %d - userID %d: %v", groupID, userID, err)
 	}
 
 	return userID, nil
+}
+
+func md5FromString(str string) string {
+	h := md5.New()
+	io.WriteString(h, str)
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+// FileData maps to files schema
+type FileData struct {
+	Path string
+	MD5  string
+}
+
+func listFiles(conf ConnConf, username string) ([]FileData, error) {
+	db, err := conf.conn()
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to database: %v", err)
+	}
+
+	var groupID int64
+	err = db.QueryRow("SELECT id FROM groups WHERE groupname = $1", username).Scan(&groupID)
+	if err != nil {
+		return nil, fmt.Errorf("could not retrieve groupID: %v", err)
+	}
+
+	rows, err := db.Query("SELECT filepath, md5 FROM files WHERE ownerID = $1", groupID)
+	if err != nil {
+		return nil, fmt.Errorf("could not retrieve files: %v", err)
+	}
+	defer rows.Close()
+
+	data := []FileData{}
+	for rows.Next() {
+		var (
+			filepath string
+			md5hash  string
+		)
+		if err := rows.Scan(&filepath, &md5hash); err != nil {
+			return nil, fmt.Errorf("could not read results of query: %v", err)
+		}
+
+		data = append(data, FileData{filepath, md5hash})
+	}
+
+	return data, nil
+}
+
+func addFile(conf ConnConf, filepath string, username string) (string, error) {
+	db, err := conf.conn()
+	if err != nil {
+		return "", fmt.Errorf("could not connect to database: %v", err)
+	}
+
+	var groupID int64
+	err = db.QueryRow("SELECT id FROM groups WHERE groupname = $1", username).Scan(&groupID)
+	if err != nil {
+		return "", fmt.Errorf("could not retrieve groupID: %v", err)
+	}
+
+	var md5hash string
+	err = db.QueryRow(
+		"INSERT INTO files (filepath, md5, ownerID) VALUES ($1, $2, $3) RETURNING md5",
+		filepath, md5FromString(filepath), groupID,
+	).Scan(&md5hash)
+	if err != nil {
+		return "", fmt.Errorf("could not register file: %v", err)
+	}
+
+	return md5hash, nil
+}
+
+func getFilepath(conf ConnConf, md5hash, username string) (string, error) {
+	db, err := conf.conn()
+	if err != nil {
+		return "", fmt.Errorf("could not connect to database: %v", err)
+	}
+
+	var groupID int64
+	err = db.QueryRow("SELECT id FROM groups WHERE groupname = $1", username).Scan(&groupID)
+	if err != nil {
+		return "", fmt.Errorf("could not retrieve groupID: %v", err)
+	}
+
+	var filepath string
+	err = db.QueryRow("SELECT filepath FROM files WHERE md5 = $1 AND ownerID = $2", md5hash, groupID).Scan(&filepath)
+	if err != nil {
+		return "", fmt.Errorf("could not retrieve filepath: %v", err)
+	}
+
+	return filepath, nil
 }
